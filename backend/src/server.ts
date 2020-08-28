7ddc62631502bf43711166de3bb549dc3e79877f
Bernardo Sertório <besertorio@gmail.com>
2020-08-28 20:33:35 -0300

    :construction: Docker and database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello!' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🏄🏽‍♂️ Server started on port 3333!');
