27cf77b08748fcfe3616752bad15ba3bf36e4908
Bernardo Sertório <besertorio@gmail.com>
2020-09-07 19:47:38 -0300

    sessions logon, middlewares and image upload

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd9741bf50d712bdd853941e65e58c971',
+    expiresIn: '1d',
+  },
+};
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -3,9 +3,13 @@ import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import CreateAppointmentService from '../services/CreateAppointmentService'
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 
 appointmentsRouter.get('/', async (request, response) => {
