3470dbe548c0ed90b85116896ff130597c2eba23
Bernardo Sertório <besertorio@gmail.com>
2020-09-09 16:23:58 -0300

    image upload feature and Errors handling

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateAppointmentService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -10,7 +11,6 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
     const { name, email, password } = request.body;
 
     const createUser = new CreateUserService();
@@ -24,13 +24,19 @@ usersRouter.post('/', async (request, response) => {
     delete user.password;
 
     return response.json(user)
-  } catch (err) {
-    return response.status(400).json({ error: err.message})
-  }
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  return response.json({ok: true});
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
 });
 
 export default usersRouter;
