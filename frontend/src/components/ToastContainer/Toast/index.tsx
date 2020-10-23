46f8a0b1c6a065d945d27b08ed95bdecb5760bd7
Bernardo Sertório <besertorio@gmail.com>
2020-10-23 19:02:42 -0300

    add Toasts animations

--- frontend/src/components/ToastContainer/Toast/index.tsx
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -10,15 +10,16 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
   info: <FiInfo size={24} />,
   error: <FiAlertCircle size={24} />,
-  success: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -32,7 +33,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
