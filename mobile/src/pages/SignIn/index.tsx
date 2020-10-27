68dbccf5672321de6a31097c6c3d6610fd647ba2
Bernardo Sertório <besertorio@gmail.com>
2020-10-27 20:14:33 -0300

    pixel density

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
-const SignIn: React.FC = () => <Container />;
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+  </Container>
+);
 
 export default SignIn;
