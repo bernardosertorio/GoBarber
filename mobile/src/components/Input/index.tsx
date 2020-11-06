0cd6d5e53ee5c70d0ea11f40347b3c428b1597d8
Bernardo Sertório <besertorio@gmail.com>
2020-11-05 22:46:26 -0300

    validation of forms

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -25,13 +25,11 @@ interface InputRef {
 
 const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   { name, icon, ...rest },
-  ref
+  ref,
 ) => {
   const inputElementRef = useRef<any>(null);
 
-  const {
- registerField, defaultValue = '', fieldName, error
-} = useField(name);
+  const { registerField, defaultValue = "", fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
   const [isFocused, setIsFocused] = useState(false);
@@ -57,24 +55,24 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
     registerField<string>({
       name: fieldName,
       ref: inputValueRef.current,
-      path: "value",
+      path: 'value',
       setValue(ref: any, value) {
         inputValueRef.current.value = value;
         inputElementRef.current.setNativeProps({ text: value });
       },
       clearValue() {
-        inputValueRef.current.value = '';
+        inputValueRef.current.value = "";
         inputElementRef.current.clear();
       },
     });
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
-        color={isFocused || isFilled ? "#ff9000" : "#666360"}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
       />
 
       <TextInput
