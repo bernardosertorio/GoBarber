387c5d8671b3acade7a5a33fd0251dbb0de8133c
Bernardo Sertório <besertorio@gmail.com>
2020-11-16 13:11:36 -0300

    configuring imports

--- backend/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ backend/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,8 @@
 import { verify } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-import authConfig from '../config/auth';
 
-import AppError from '../errors/AppError';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
 
 interface TokenPayload {
   iat: number;
