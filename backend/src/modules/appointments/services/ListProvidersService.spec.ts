7bdc9eec9dc54b36448415861628348b86690f8d
Bernardo Sertório <besertorio@gmail.com>
2020-11-24 17:22:55 -0300

    list of providers

---
+++ backend/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Tre',
+      email: 'jhonTre@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = fakeUsersRepository.create({
+      name: 'John Qua',
+      email: 'jhonQua@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
