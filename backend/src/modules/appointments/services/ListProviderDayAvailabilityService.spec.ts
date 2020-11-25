e21ee73a3ff2c905ce87bf3f8e4da5265bff4bd1
Bernardo Sertório <besertorio@gmail.com>
2020-11-25 19:08:49 -0300

    excluding old times

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 21, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 21, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -33,9 +37,12 @@ describe('ListProviderMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { day: 8, available: false },
-        { day: 9, available: true },
+        { day: 9, available: false },
         { day: 10, available: false },
-        { day: 11, available: true },
+        { day: 13, available: true },
+        { day: 14, available: false },
+        { day: 15, available: false },
+        { day: 16, available: true },
       ]),
     );
   });
