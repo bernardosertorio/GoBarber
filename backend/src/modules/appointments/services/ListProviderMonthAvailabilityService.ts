92701303d5ceb48f7b94ab8f498ab8a6da509781
Bernardo Sertório <besertorio@gmail.com>
2020-11-25 16:36:24 -0300

    filtering schedules by month

---
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,40 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('AppointsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProvidersService;
