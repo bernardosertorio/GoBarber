27f0e4fd209191573d97c600610e82e18a75a5d0
Bernardo Sertório <besertorio@gmail.com>
2020-12-09 20:42:56 -0300

    next appointment

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,68 @@
 import React from 'react';
 
-const Dashboard: React.FC = () =>();
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <strong>Bernardo</strong>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
