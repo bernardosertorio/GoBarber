ac30821d02de21e7ff670beb1d63a5acb8c79fce
Bernardo Sertório <besertorio@gmail.com>
2020-12-15 18:45:28 -0300

    creating pages and routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Dashboard from '../pages/Dashboard';
+import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
+import CreateAppointment from "../pages/CreateAppointment";
+import AppointmentCreated from "../pages/AppointmentCreated";
 
 const App = createStackNavigator();
 
@@ -9,10 +12,14 @@ const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
       headerShown: false,
-      cardStyle: { backgroundColor: "#312e38" },
+      cardStyle: { backgroundColor: '#312e38' },
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
